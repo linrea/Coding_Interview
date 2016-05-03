e5649f68d3400c97de0bc962a666a45fdc505e64
Lei Lin <234811990@qq.com>
2016-05-02 22:49:29 -0400

    Added files via upload

---
+++ inorderTraversal.java
@@ -0,0 +1,53 @@
+package codinginterview;
+
+import java.util.Stack;
+
+
+
+public class inorderTraversal
+{
+	public static void inorderTraversal(TreeNode root) {
+
+        if(root == null)
+	            return;
+	
+	        Stack<TreeNode> st=new Stack<TreeNode>();
+	
+	        while(!st.empty()||root!=null ){
+	
+	/* The 3 Nodes: root, left and right can actually be regarded as 3 subtrees( same type ), though we did little optimization here so we only push 1 subtree instead of 3 at one time( always the root ).*/
+	
+	            if(root!=null){
+	                st.push(root);
+	                root = root.left;
+	            }else{
+	                root = st.pop();	                
+	                System.out.println(root.val);
+	                root = root.right;
+	            }
+	      }
+	}
+	public static void main(String[] args)
+	{
+		TreeNode root = new TreeNode(2);
+		TreeNode newNodeB = new TreeNode(5);
+		TreeNode newNodeC = new TreeNode(10);
+		TreeNode newNodeD = new TreeNode(6);
+		TreeNode newNodeE = new TreeNode(20);
+		TreeNode newNodeF = new TreeNode(20);
+		TreeNode newNodeG = new TreeNode(13);
+		TreeNode newNodeH = new TreeNode(4);
+		TreeNode newNodeI = new TreeNode(1);
+		root.left = newNodeB;
+		root.right = newNodeC;
+		root.right.left = newNodeD;
+		root.right.right= newNodeE;
+		root.left.right = newNodeF;
+		root.left.left = newNodeG;
+		root.right.right.left = newNodeH;
+		root.right.right.right = newNodeI;
+		inorderTraversal(root);
+		
+
+	}
+}
